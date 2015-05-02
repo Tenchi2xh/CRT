ce59eb59a260a9b7bc431a91507f500a31d9c70b
Hamza Haiken <tenchi@team2xh.net>
2015-05-02 15:56:34 +0200

    Cont.

--- src/main/java/net/team2xh/crt/gui/MainWindow.java
+++ src/main/java/net/team2xh/crt/gui/MainWindow.java
@@ -16,9 +16,15 @@
  */
 package net.team2xh.crt.gui;
 
+import bibliothek.extension.gui.dock.preference.PreferenceDialog;
+import bibliothek.extension.gui.dock.preference.PreferenceModel;
+import bibliothek.extension.gui.dock.preference.PreferenceTreeDialog;
+import bibliothek.extension.gui.dock.preference.PreferenceTreeModel;
+import bibliothek.gui.DockController;
 import bibliothek.gui.dock.common.CContentArea;
 import bibliothek.gui.dock.common.CControl;
 import bibliothek.gui.dock.common.CGrid;
+import bibliothek.gui.dock.common.CPreferenceModel;
 import bibliothek.gui.dock.common.ColorMap;
 import bibliothek.gui.dock.common.DefaultSingleCDockable;
 import bibliothek.gui.dock.common.intern.CDockable;
@@ -26,15 +32,16 @@ import bibliothek.gui.dock.common.intern.DefaultCDockable;
 import bibliothek.gui.dock.common.theme.ThemeMap;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.util.Locale;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 import net.team2xh.crt.gui.editor.Editor;
 import net.team2xh.crt.gui.entities.EntityTree;
-import net.team2xh.crt.gui.themes.SubstanceTheme;
 import net.team2xh.crt.gui.themes.Theme;
 import net.team2xh.crt.raytracer.Scene;
 
@@ -46,12 +53,12 @@ public class MainWindow extends JFrame {
 
     private final CControl control;
     private final Theme theme;
-    
-    private final Editor editorComponent;
-    private final EntityTree navigatorComponent;
-    private final JLabel renderComponent;
-    private final JLabel systemComponent;
-    private final JLabel settingsComponent;
+
+    private final Editor editor;
+    private final EntityTree navigator;
+    private final JLabel render;
+    private final JLabel system;
+    private final JLabel settings;
 
     public MainWindow(Theme theme) {
 
@@ -61,55 +68,78 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
         setMinimumSize(new Dimension(800, 600));
-        
+
         ThemeMap themes = control.getThemes();
         themes.select(ThemeMap.KEY_ECLIPSE_THEME);
 
+        DockController controller = control.getController();
+//        controller.getThemeManager().setBorderModifier(null, null);
+
+        PreferenceModel model = new CPreferenceModel(control);
+        control.setPreferenceModel(model);
+        Component owner = control.intern().getController().findRootWindow();
+        if (model instanceof PreferenceTreeModel) {
+            PreferenceTreeModel tree = (PreferenceTreeModel) model;
+            PreferenceTreeDialog.openDialog(tree, owner);
+        } else {
+            PreferenceDialog.openDialog(model, owner);
+        }
+
         CContentArea contentArea = control.getContentArea();
         add(contentArea, BorderLayout.CENTER);
-        
-        editorComponent = new Editor(theme);
-        navigatorComponent = new EntityTree();
-        renderComponent = new JLabel("Render goes here");
-        systemComponent = new JLabel("Infos go here");
-        settingsComponent = new JLabel("Settings go here");
 
-        DefaultCDockable editor = (DefaultCDockable) create("Script", editorComponent);
-        DefaultCDockable navigator = (DefaultCDockable) create("Navigator", navigatorComponent);
-        DefaultCDockable render = (DefaultCDockable) create("Render", renderComponent);
-        DefaultCDockable system = (DefaultCDockable) create("System informations", systemComponent);
-        DefaultCDockable settings = (DefaultCDockable) create("Settings", settingsComponent);
+        editor = new Editor(theme);
+        navigator = new EntityTree();
+        render = new JLabel("Render goes here");
+        system = new JLabel("Infos go here");
+        settings = new JLabel("Settings go here");
+
+        DefaultCDockable dEditor = (DefaultCDockable) create("Script", editor);
+        DefaultCDockable dNavigator = (DefaultCDockable) create("Navigator", navigator);
+        DefaultCDockable dRender = (DefaultCDockable) create("Render", render);
+        DefaultCDockable dSystem = (DefaultCDockable) create("System informations", system);
+        DefaultCDockable dSettings = (DefaultCDockable) create("Settings", settings);
+
+        dRender.setExternalizable(true);
+        // TODO: see how to minimize on the left or right
 
         CGrid grid = new CGrid(control);
-        grid.add(0, 0, 0.5, 0.75, render);
-        grid.add(0.5, 0, 0.25, 0.75, editor);
-        grid.add(0.75, 0, 0.25, 1, navigator);
-        grid.add(0, 0.75, 0.375, 0.25, system);
-        grid.add(0.375, 0.75, 0.375, 0.25, settings);
+        grid.add(0, 0, 0.5, 0.75, dRender);
+        grid.add(0.5, 0, 0.25, 0.75, dEditor);
+        grid.add(0.75, 0, 0.25, 1, dNavigator);
+        grid.add(0, 0.75, 0.375, 0.25, dSystem);
+        grid.add(0.375, 0.75, 0.375, 0.25, dSettings);
         contentArea.deploy(grid);
 
         setVisible(true);
     }
-    
+
     public void loadScene(Scene scene) {
-        navigatorComponent.loadScene(scene);
+        navigator.loadScene(scene);
     }
 
     private CDockable create(String title, JComponent component) {
         DefaultCDockable dockable = new DefaultSingleCDockable(title, title, component);
         dockable.setCloseable(false);
-        
+        dockable.setExternalizable(false);
+
         ColorMap map = dockable.getColors();
         map.setColor(ColorMap.COLOR_KEY_TITLE_BACKGROUND, UIManager.getColor("TextArea.background", null));
         map.setColor(ColorMap.COLOR_KEY_MINIMIZED_BUTTON_BACKGROUND, UIManager.getColor("TextArea.background", null));
         map.setColor(ColorMap.COLOR_KEY_TAB_BACKGROUND, UIManager.getColor("TextArea.background", null));
-        
+
         map.setColor(ColorMap.COLOR_KEY_TITLE_BACKGROUND_FOCUSED, UIManager.getColor("TextArea.selectionBackground", null));
         map.setColor(ColorMap.COLOR_KEY_MINIMIZED_BUTTON_BACKGROUND_FOCUSED, UIManager.getColor("TextArea.selectionBackground", null));
         map.setColor(ColorMap.COLOR_KEY_MINIMIZED_BUTTON_BACKGROUND_SELECTED, UIManager.getColor("TextArea.selectionBackground", null));
         map.setColor(ColorMap.COLOR_KEY_TAB_BACKGROUND_FOCUSED, UIManager.getColor("TextArea.selectionBackground", null));
 //        map.setColor(ColorMap.COLOR_KEY_TAB_BACKGROUND_SELECTED, UIManager.getColor("TextArea.selectionBackground", null));
-        
+
+        map.setColor(ColorMap.COLOR_KEY_TAB_FOREGROUND_FOCUSED, Color.RED);
+        map.setColor(ColorMap.COLOR_KEY_TAB_FOREGROUND_SELECTED, Color.BLACK);
+        map.setColor(ColorMap.COLOR_KEY_TAB_FOREGROUND, Color.ORANGE);
+        map.setColor(ColorMap.COLOR_KEY_TITLE_FOREGROUND, Color.CYAN);
+        map.setColor(ColorMap.COLOR_KEY_TITLE_FOREGROUND_FOCUSED, Color.PINK);
+
         return dockable;
     }
 
